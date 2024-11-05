629612cbe018b4afa999d3e9922304b72afb840f
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2024-11-05 20:06:32 +0530

    feat: gives product a navigation bar on top

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import "./App.css";
+
+function App() {
+    return (
+        <Router>
+            <Navbar />
+        </Router>
+    );
+}
+
+export default App;
